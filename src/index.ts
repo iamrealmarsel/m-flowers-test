4e838820c119f517c082a803ed8d3428fe252d6c
mars <maspam@yandex.ru>
2022-12-03 00:27:54 +0300

    feat: add adaptive

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 import './styles/main.scss';
 
 console.log('hello, world !');
+
+const burger = document.querySelector('.js-burger') as HTMLElement;
+const menu = document.querySelector('.js-menu') as HTMLElement;
+const overlay = document.querySelector('.js-overlay') as HTMLElement;
+
+burger.addEventListener('click', () => {
+  menu.classList.add('_active');
+  overlay.classList.add('_active');
+});
+
+overlay.addEventListener('click', () => {
+  menu.classList.remove('_active');
+  overlay.classList.remove('_active');
+});
